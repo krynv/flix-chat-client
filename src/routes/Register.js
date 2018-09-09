793d32beda7cbd436b50d26da7310d4ba3cb2b3c
VK <vitaliy.krynytskyy@gmail.com>
2018-09-09 23:07:40 +0100

    Added validation for registering a new user

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Input, Container, Header } from 'semantic-ui-react';
+import { Message, Button, Input, Container, Header } from 'semantic-ui-react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
@@ -7,8 +7,11 @@ class Register extends React.Component {
 
     state = {
         username: '',
+        usernameError: '',
         email: '',
-        password: ''
+        emailError: '',
+        password: '',
+        passwordError: '',
     };
 
     // one onChange function
@@ -18,32 +21,80 @@ class Register extends React.Component {
     }
 
     onSubmit = async () => {
-        const response = await this.props.mutate({
-            variables: this.state,
+
+        // reset errors again after input
+        this.setState({
+            usernameError: '',
+            emailError: '',
+            passwordError: '',
         })
+
+        const { username, email, password } = this.state;
+        const response = await this.props.mutate({
+            variables: { username, email, password },
+        });
+
+        const { ok, errors } = response.data.register;
+
+        if (ok) {
+            this.props.history.push('/');
+        } else {
+            const err = {};
+
+            errors.forEach(({ path, message }) => {
+                err[`${path}Error`] = message;
+            });
+
+            this.setState(err);
+        }
+
         console.log(response);
     }
 
     render() {
 
-        const { username, email, password } = this.state;
+        const { username, email, password, usernameError, emailError, passwordError } = this.state;
+
+        const errorList = [];
+
+        if (usernameError) {
+            errorList.push(usernameError);
+        }
+        if (emailError) {
+            errorList.push(emailError);
+        }
+        if (passwordError) {
+            errorList.push(passwordError);
+        }
 
         return (
             <Container text>
                 <Header as='h2'>Register</Header>
-                <Input name="username" onChange={this.onChange} value={username} placeholder="Username" fluid></Input>
-                <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid></Input>
-                <Input name="password" onChange={this.onChange} value={password} type="password" placeholder="Password" fluid></Input>
+                <Input error={!!usernameError} name="username" onChange={this.onChange} value={username} placeholder="Username" fluid></Input>
+                <Input error={!!emailError} name="email" onChange={this.onChange} value={email} placeholder="Email" fluid></Input>
+                <Input error={!!passwordError} name="password" onChange={this.onChange} value={password} type="password" placeholder="Password" fluid></Input>
                 <Button onClick={this.onSubmit}>Submit</Button>
+                {(usernameError || emailError || passwordError) ?
+                    <Message
+                        error
+                        header="There were issues with registering your flix-chat account."
+                        list={errorList}
+                    /> : null}
             </Container>
         );
     }
 }
 
 const registerMutation = gql`
-    mutation ($username: String!, $email: String!, $password: String!) {
-        register(username: $username, email: $email, password: $password)
+mutation ($username: String!, $email: String!, $password: String!) {
+    register(username: $username, email: $email, password: $password) {
+        ok
+        errors {
+            path
+            message
+        }
     }
+}
 `;
 
 export default graphql(registerMutation)(Register);
